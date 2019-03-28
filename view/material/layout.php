1d43505f14046a07357d609e1ec4fd1fa7e3e232
pangmao100 <48122116+pangmao100@users.noreply.github.com>
2019-03-28 13:10:26 +0800

    修改默认模板

--- view/material/layout.php
+++ view/material/layout.php
@@ -3,16 +3,22 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no"/>
-	<title><?php e($title.' - '.config('site_name'));?></title>
+	<title><?php e($title.''.config('site_name'));?></title>
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mdui@0.4.1/dist/css/mdui.min.css" integrity="sha256-lCFxSSYsY5OMx6y8gp8/j6NVngvBh3ulMtrf4SX5Z5A=" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/mdui@0.4.1/dist/js/mdui.min.js" integrity="sha256-dZxrLDxoyEQADIAGrWhPtWqjDFvZZBigzArprSzkKgI=" crossorigin="anonymous"></script>
 	<style>
+	    .mdui-typo-display-2-opacity {
+            font-size: 38px;
+            font-weight: 400;
+            letter-spacing: 0;
+	    }
 		.mdui-appbar .mdui-toolbar{
 			height:56px;
 			font-size: 16px;
 		}
 		.mdui-toolbar>*{
-			padding: 0 6px;
+			padding: 0 2px;
 			margin: 0 2px;
 			opacity:0.5;
 		}
@@ -48,33 +54,32 @@
 		}
 		@media screen and (max-width:980px){
 			.mdui-list-item .mdui-text-right{
-				display: none;
-			}
-			.mdui-container{
-				width:100% !important;
-				margin:0px;
-			}
-			.mdui-toolbar>*{
-				display: none;
-			}
-			.mdui-toolbar>a:last-child,.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>i:first-child{
-				display: block;
-			}
+			display: none;
+		}
+		.mdui-container{
+			width:100% !important;
+			margin:0px;
+		}
+		.mdui-toolbar>*{
+			display: none;
+		}
+		.mdui-toolbar>a:last-child,.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>i:first-child{
+			display: block;
 		}
 	</style>
 </head>
 <body class="mdui-theme-primary-blue-grey mdui-theme-accent-blue">
-	<header class="mdui-appbar mdui-color-theme">
+	<header class="mdui-appbar mdui-color-theme mdui-appbar-fixed mdui-appbar-scroll-hide">
 		<div class="mdui-toolbar mdui-container">
 			<a href="/" class="mdui-typo-headline"><?php e(config('site_name'));?></a>
-			<?php foreach((array)$navs as $n=>$l):?>
+			<?php $navs=(array)$navs;array_splice($navs, 0, 1);foreach($navs as $n=>$l):?>
 			<i class="mdui-icon material-icons mdui-icon-dark" style="margin:0;">chevron_right</i>
 			<a href="<?php e($l);?>"><?php e($n);?></a>
-			<?php endforeach;?>
-			<!--<a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">refresh</i></a>-->
+			<?php endforeach;?>	
+            <div class="mdui-toolbar-spacer"></div>
+            <a href="/admin" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">build</i></a>		
 		</div>
-	</header>
-	
+	</header>	
 	<div class="mdui-container">
     	<?php view::section('content');?>
   	</div>
--- view/material/list.php
+++ view/material/list.php
@@ -11,8 +11,14 @@ function file_ico($item){
   if(in_array($ext,['ogg','mp3','wav'])){
   	return "audiotrack";
   }
+    if(in_array($ext,['zip','rar','iso','7z'])){
+  	return "storage";
+  }
+  if(in_array($ext,['pdf'])){
+  	return "picture_as_pdf";
+  }
   return "insert_drive_file";
-}
+  }
 ?>
 
 <?php view::begin('content');?>
@@ -23,22 +29,22 @@ function file_ico($item){
 <div class="mdui-typo" style="padding: 20px;">
 	<?php e($head);?>
 </div>
-<?php endif;?>
-
-	
+<?php endif;?>	
+<br></br>
+<br></br>
 <div class="mdui-row">
 	<ul class="mdui-list">
 		<li class="mdui-list-item th">
-		  <div class="mdui-col-xs-12 mdui-col-sm-7">文件 <i class="mdui-icon material-icons icon-sort" data-sort="name" data-order="downward">expand_more</i></div>
+		  <div class="mdui-col-xs-12 mdui-col-sm-7">名称 <i class="mdui-icon material-icons icon-sort" data-sort="name" data-order="downward">expand_more</i></div>
 		  <div class="mdui-col-sm-3 mdui-text-right">修改时间 <i class="mdui-icon material-icons icon-sort" data-sort="date" data-order="downward">expand_more</i></div>
 		  <div class="mdui-col-sm-2 mdui-text-right">大小 <i class="mdui-icon material-icons icon-sort" data-sort="size" data-order="downward">expand_more</i></div>
 		</li>
 		<?php if($path != '/'):?>
-		<li class="mdui-list-item mdui-ripple">
+		<li class="mdui-list-item mdui-ripple mdui-ripple-blue">
 			<a href="<?php echo get_absolute_path($root.$path.'../');?>">
 			  <div class="mdui-col-xs-12 mdui-col-sm-7">
-				<i class="mdui-icon material-icons">arrow_upward</i>
-		    	..
+				<i class="mdui-icon material-icons">reply</i>
+		    	<b>...</b>
 			  </div>
 			  <div class="mdui-col-sm-3 mdui-text-right"></div>
 			  <div class="mdui-col-sm-2 mdui-text-right"></div>
@@ -49,7 +55,7 @@ function file_ico($item){
 		<?php foreach((array)$items as $item):?>
 			<?php if(!empty($item['folder'])):?>
 
-		<li class="mdui-list-item mdui-ripple" data-sort data-sort-name="<?php e($item['name']);?>" data-sort-date="<?php echo $item['lastModifiedDateTime'];?>" data-sort-size="<?php echo $item['size'];?>">
+		<li class="mdui-list-item mdui-ripple mdui-ripple-blue" data-sort data-sort-name="<?php e($item['name']);?>" data-sort-date="<?php echo $item['lastModifiedDateTime'];?>" data-sort-size="<?php echo $item['size'];?>">
 			<a href="<?php echo get_absolute_path($root.$path.rawurlencode($item['name']));?>">
 			  <div class="mdui-col-xs-12 mdui-col-sm-7 mdui-text-truncate">
 				<i class="mdui-icon material-icons">folder_open</i>
@@ -60,7 +66,7 @@ function file_ico($item){
 		  	</a>
 		</li>
 			<?php else:?>
-		<li class="mdui-list-item file mdui-ripple" data-sort data-sort-name="<?php e($item['name']);?>" data-sort-date="<?php echo $item['lastModifiedDateTime'];?>" data-sort-size="<?php echo $item['size'];?>">
+		<li class="mdui-list-item file mdui-ripple mdui-ripple-blue" data-sort data-sort-name="<?php e($item['name']);?>" data-sort-date="<?php echo $item['lastModifiedDateTime'];?>" data-sort-size="<?php echo $item['size'];?>">
 			<a href="<?php echo get_absolute_path($root.$path).rawurlencode($item['name']);?>" target="_blank">
 			  <div class="mdui-col-xs-12 mdui-col-sm-7 mdui-text-truncate">
 				<i class="mdui-icon material-icons"><?php echo file_ico($item);?></i>
@@ -75,15 +81,35 @@ function file_ico($item){
 	</ul>
 </div>
 <?php if($readme):?>
-<div class="mdui-typo mdui-shadow-3" style="padding: 20px;margin: 20px 0;">
-	<div class="mdui-chip">
-	  <span class="mdui-chip-icon"><i class="mdui-icon material-icons">face</i></span>
-	  <span class="mdui-chip-title">README.md</span>
-	</div>
+<div class="mdui-typo mdui-shadow-0" style="padding: 10px 0;margin: 10px 0;">
 	<?php e($readme);?>
 </div>
 <?php endif;?>
+<div class="mdui-container mdui-p-t-0">
+  <button class="mdui-btn mdui-color-theme-accent" mdui-menu="{target: '#example-3'}">联系胖猫</button>
+  <ul class="mdui-menu mdui-menu-cascade" id="example-3">
+    <li class="mdui-menu-item">
+      <a href="javascript:;" class="mdui-ripple">
+        <i class="mdui-menu-item-icon mdui-icon material-icons">smartphone</i>
+        15982142404
+      </a>
+    </li>
+    <li class="mdui-menu-item">
+      <a href="javascript:;" class="mdui-ripple">
+        <i class="mdui-menu-item-icon mdui-icon material-icons">mail_outline</i>
+        12898152@qq.com
+      </a>
+    </li>
+    <li class="mdui-menu-item">
+      <a href="javascript:;" class="mdui-ripple">
+	    <i class="mdui-menu-item-icon mdui-icon material-icons">room</i>
+        四川省彭州市
+        <!--<span class="mdui-menu-item-helper">右边</span>-->
+      </a>
+    </li>
+  </ul>
 </div>
+<br></br>
 <script>
 $ = mdui.JQ;
 
